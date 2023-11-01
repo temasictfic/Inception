e3750c31d679f4fdb49f1d291aef0f1b69defe7b
temasictfic <76123805+temasictfic@users.noreply.github.com>
2023-11-01 15:05:40 +0300

    initial

---
+++ Makefile
@@ -0,0 +1,54 @@
+DOCKER = sudo docker
+COMPOSE = $(DOCKER) compose -p inception -f srcs/docker-compose.yml
+MARIADB_VOLUME = /home/${DB_USER_NAME}/data/mariadb
+WORDPRESS_VOLUME = /home/${WP_ADMIN_NAME}/data/wordpress
+DEPENDENCIES = $(MARIADB_VOLUME) $(WORDPRESS_VOLUME)
+
+all: up
+
+$(MARIADB_VOLUME):
+	mkdir -p $(MARIADB_VOLUME)
+
+$(WORDPRESS_VOLUME):
+	mkdir -p $(WORDPRESS_VOLUME)
+
+ps:
+	$(COMPOSE) ps
+
+images:
+	$(COMPOSE) images
+
+volumes:
+	$(DOCKER) volume ls
+
+networks:
+	$(DOCKER) network ls
+
+start: $(DEPENDENCIES)
+	$(COMPOSE) start
+
+stop:
+	$(COMPOSE) stop
+
+restart: $(DEPENDENCIES)
+	$(COMPOSE) restart
+
+up: $(DEPENDENCIES)
+	$(COMPOSE) up --detach --build
+
+down:
+	$(COMPOSE) down
+
+clean:
+	$(COMPOSE) down --rmi all --volumes
+
+fclean: clean
+	sudo rm -rf $(DEPENDENCIES) 
+
+prune: down fclean
+	$(DOCKER) system prune -a -f
+
+re: fclean all
+
+.PHONY: all ps images volumes networks start stop restart up down clean fclean prune re
+
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "== Installing and setting up Wordpress =="
+
+cd /var/www/html/wordpress
+
+# Download
+wp core download --path=/var/www/html/wordpress --allow-root
+
+# Create config file wp-config.php with the appropriate database parameters (these are env variables in the .env file)
+wp config create --path=/var/www/html/wordpress --allow-root --dbname=$DB_DATABASE --dbhost=$DB_HOST --dbprefix=wp_ --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD
+
+# Install wordpress for our website (again, variables are in the .env file)
+wp core install --path=/var/www/html/wordpress --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+wp plugin update --path=/var/www/html/wordpress --allow-root --all
+
+# Create default user
+wp user create --path=/var/www/html/wordpress --allow-root $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+
+# Set the owner of the content of our site to www-data user and group
+# For security reasons, we want to restrict who has access to these files
+chown www-data:www-data /var/www/html/wordpress/wp-content/uploads --recursive
+
+mkdir -p /run/php/
+php-fpm8.2 -F
