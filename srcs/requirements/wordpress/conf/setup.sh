942c37c54be936b1b1d86a851ad60e9e864684d5
temasictfic <76123805+temasictfic@users.noreply.github.com>
2023-11-01 17:05:48 +0300

    new conf

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,36 +2,25 @@
 
 echo "== Installing and setting up Wordpress =="
 
-WP_DIR="/var/www/wordpress/"
-WP_CONF="wp-config.php"
+cd /var/www/wordpress
 
-if [ ! -d $WP_DIR ]; then
-    mkdir -p $WP_DIR
-fi
+# Download
+wp core download --path=/var/www/wordpress --allow-root
 
-if [ -d $WP_DIR ]; then
-    cd $WP_DIR
-    # Download
-    wp --allow-root core download
+# Create config file wp-config.php with the appropriate database parameters (these are env variables in the .env file)
+wp config create --path=/var/www/wordpress --allow-root --dbname=$DB_DATABASE --dbhost=$DB_HOST --dbprefix=wp_ --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD
 
-    if [ ! -f $WP_DIR$WP_CONF ]; then
-        wp --allow-root config create --dbname=$DB_DATABASE --dbhost=$DB_HOST --dbprefix=wp_ --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD
-    fi
-    # Create config file wp-config.php with the appropriate database parameters (these are env variables in the .env file)
-    wp config create --allow-root --dbname=$DB_DATABASE --dbhost=$DB_HOST --dbprefix=wp_ --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD
+# Install wordpress for our website (again, variables are in the .env file)
+wp core install --path=/var/www/wordpress --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
-    # Install wordpress for our website (again, variables are in the .env file)
-    wp core install --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+wp plugin update --path=/var/www/wordpress --allow-root --all
 
-    wp plugin update --allow-root --all
+# Create default user
+wp user create --path=/var/www/wordpress --allow-root $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 
-    # Create default user
-    wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+# Set the owner of the content of our site to www-data user and group
+# For security reasons, we want to restrict who has access to these files
+chown www-data:www-data /var/www/wordpress/wp-content/uploads --recursive
 
-    # Set the owner of the content of our site to www-data user and group
-    # For security reasons, we want to restrict who has access to these files
-    chown -R www-data:www-data $WP_DIR
-fi
-
-#mkdir -p /run/php/
-php-fpm8.2 --nodaemonize
+mkdir -p /run/php/
+php-fpm8.2 -F
