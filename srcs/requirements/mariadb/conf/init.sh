c27654eb886bbf27f811d37c67ca0269dcba2a9d
temasictfic <76123805+temasictfic@users.noreply.github.com>
2023-11-02 17:48:43 +0300

    secure

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-mysql -uroot -p$DB_ROOT_PASSWORD<<EOF
+mysqld_safe &;
+
+mysql -uroot <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+EOF;
+
+kill `cat /var/run/mysqld/mysqld.pid`;
+
+service start mariadb;
+
+mysql -uroot -p$DB_ROOT_PASSWORD <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_DATABASE;
 CREATE USER IF NOT EXISTS '$DB_USER_NAME'@localhost IDENTIFIED BY '$DB_USER_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER_NAME'@localhost;
