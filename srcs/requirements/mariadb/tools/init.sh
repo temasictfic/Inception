ceb6551e5add301e7e0f4552a7b283f86840f111
Sciftci <temasictfic@gmail.com>
2023-11-06 18:15:13 +0300

    myscript

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -11,9 +11,12 @@ kill `cat /var/run/mysqld/mysqld.pid`
 service start mariadb
 
 mysql -uroot -p$DB_ROOT_PASSWORD <<EOF
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $DB_DATABASE;
 CREATE USER IF NOT EXISTS '$DB_USER_NAME'@localhost IDENTIFIED BY '$DB_USER_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER_NAME'@localhost;
 FLUSH PRIVILEGES;
 EOF
+
+service stop mariadb
+
+mysqld
